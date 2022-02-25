32212bed2d1d309ecdaef393b944ab6f06c8b36f
Hui Chen <alex.chenhui@gmail.com>
2022-02-25 21:09:26 +0800

    add service

--- spring/springBoot/spaceship/src/main/java/com/nasa/SpaceshipApplication.java
+++ spring/springBoot/spaceship/src/main/java/com/nasa/SpaceshipApplication.java
@@ -1,9 +1,13 @@
 package com.nasa;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.nasa"})
+@MapperScan("com.nasa.mapper")
 public class SpaceshipApplication {
 
 	public static void main(String[] args) {
--- spring/springBoot/spaceship/src/main/java/com/nasa/mapper/UserMapper.java
+++ spring/springBoot/spaceship/src/main/java/com/nasa/mapper/UserMapper.java
@@ -1,5 +1,10 @@
 package com.nasa.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
 import com.nasa.model.User;
 
 public interface UserMapper {
@@ -14,4 +19,13 @@ public interface UserMapper {
     int updateByPrimaryKeySelective(User record);
 
     int updateByPrimaryKey(User record);
+    
+    @Select("Select * from user")
+    List<User> selectAll();
+
+    @Select("Select * from user where username = #{username} and password = #{password}")
+    User selectByUsernamePass(@Param("username") String username, @Param("password") String password);
+
+    @Select("Select * from user where username = #{username}")
+    User selectByUsername(@Param("username") String username);    
 }
