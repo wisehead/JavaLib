ec6998812ca55553cba86b383c30b846d6a5642b
wisehead <alex.chenhui@gmail.com>
2022-03-01 17:30:20 +0800

    remove

---
+++ 01.java_basic/20.HashSet/03.remove/RunoobTest.java
@@ -0,0 +1,15 @@
+// 引入 HashSet 类      
+import java.util.HashSet;
+
+public class RunoobTest {
+    public static void main(String[] args) {
+    HashSet<String> sites = new HashSet<String>();
+        sites.add("Google");
+        sites.add("Runoob");
+        sites.add("Taobao");
+        sites.add("Zhihu");
+        sites.add("Runoob");     // 重复的元素不会被添加
+        sites.remove("Taobao");  // 删除元素，删除成功返回 true，否则为 false
+        System.out.println(sites);
+    }
+}
