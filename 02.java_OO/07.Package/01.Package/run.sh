3a4289e18b6ed8955cbd67af72c5cb18b4547ef9
wisehead <alex.chenhui@gmail.com>
2022-03-01 14:39:33 +0800

    package ok

--- 02.java_OO/07.Package/01.Package/run.sh
+++ 02.java_OO/07.Package/01.Package/run.sh
@@ -6,3 +6,12 @@
 #############################################################
 #!/bin/sh 
 java animals/MammalInt
+#Java 中带包（创建及引用）的类的编译
+
+#只有一个文件时编译：
+
+#javac A.java
+#一个包的文件都在时编译：
+
+javac -d . *.java
+#运行：编译之后会自己生成文件夹，不要进入这个文件夹，直接运行 java -cp /home/test test.Run，其中源文件在 test 文件夹中，包名为 test，启动文件为 Run.java。
