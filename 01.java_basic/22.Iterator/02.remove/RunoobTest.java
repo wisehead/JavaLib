2fa25866f90d2d00bfe62027ba53b5f2827add0a
wisehead <alex.chenhui@gmail.com>
2022-03-01 19:28:41 +0800

    iterator

---
+++ 01.java_basic/22.Iterator/02.remove/RunoobTest.java
@@ -0,0 +1,21 @@
+// 引入 ArrayList 和 Iterator 类
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class RunoobTest {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(12);
+        numbers.add(8);
+        numbers.add(2);
+        numbers.add(23);
+        Iterator<Integer> it = numbers.iterator();
+        while(it.hasNext()) {
+            Integer i = it.next();
+            if(i < 10) {  
+                it.remove();  // 删除小于 10 的元素
+            }
+        }
+        System.out.println(numbers);
+    }
+}
