12643cd5ac5a0b8ed5fd5eb64ce3aab41340fb2f
wisehead <alex.chenhui@gmail.com>
2022-03-01 09:27:42 +0800

    Iterator

---
+++ 01.java_basic/11.Enum/02.Iterator/MyClass.java
@@ -0,0 +1,11 @@
+enum Color
+{
+    RED, GREEN, BLUE;
+}
+public class MyClass {
+  public static void main(String[] args) {
+    for (Color myVar : Color.values()) {
+      System.out.println(myVar);
+    }
+  }
+}
