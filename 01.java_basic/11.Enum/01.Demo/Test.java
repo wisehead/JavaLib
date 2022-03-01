d8f646e49f762349bb7d13d271724df9f834e088
wisehead <alex.chenhui@gmail.com>
2022-03-01 09:26:29 +0800

    Demo

---
+++ 01.java_basic/11.Enum/01.Demo/Test.java
@@ -0,0 +1,14 @@
+enum Color
+{
+    RED, GREEN, BLUE;
+}
+ 
+public class Test
+{
+    // 执行输出结果
+    public static void main(String[] args)
+    {
+        Color c1 = Color.RED;
+        System.out.println(c1);
+    }
+}
