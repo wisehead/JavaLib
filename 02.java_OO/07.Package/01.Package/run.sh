2f8c819ed9a5c47b557db7f796b98fa240c89ef7
wisehead <alex.chenhui@gmail.com>
2022-03-01 14:15:23 +0800

    package

---
+++ 02.java_OO/07.Package/01.Package/run.sh
@@ -0,0 +1,8 @@
+#############################################################
+#   File Name: run.sh
+#     Autohor: Hui Chen (c) 2022
+#        Mail: chenhui13@xxx.com
+# Create Time: 2022/03/01-14:15:02
+#############################################################
+#!/bin/sh 
+java animals/MammalInt
